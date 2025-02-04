4fea54d514e899c572a9081d23f2000a27d98a9e
Hakim Syukri <hakimsyukri.mhd@gmail.com>
2025-02-04 16:30:12 +0700

    add modoles

--- node-js/routes/category.js
+++ node-js/routes/category.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const newsData = require("../data.json");
 
 router.get("/", (req, res) => {
   res.status(200).json({
@@ -6,4 +7,31 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:title", (req, res) => {
+  const newsQuery = req.query.title;
+  const newsParam = req.params.category;
+
+  if (newsQuery) {
+    let newsItem = newsData.filter((news) => {
+      return news.title.toLowerCase().includes(newsQuery.toLowerCase());
+    });
+
+    if (newsItem.length === 0) {
+      res.status(404).json({
+        message: "Berita tidak ditemukan!",
+      });
+    }
+    res.status(200).json(newsItem);
+  } else {
+    let newsItem2 = newsData.filter((news) => {
+      return news.title === newsParam;
+    });
+    if (newsItem2.length === 0) {
+      res.status(404).json({
+        message: "Berita tidak ditemukan!",
+      });
+    }
+    res.status(200).json(newsItem2);
+  }
+});
 module.exports = router;
--- node-js/routes/news.js
+++ node-js/routes/news.js
@@ -1,38 +1,9 @@
 const router = require("express").Router();
-const newsData = require("../data.json");
 const category = require("./category");
 
 router.get("/", (req, res) => {
   res.status(200).json(newsData);
 });
-
 router.use("/category", category);
-router.get("/:title", (req, res) => {
-  const newsQuery = req.query.title;
-  const newsParam = req.params.category;
-
-  if (newsQuery) {
-    let newsItem = newsData.filter((news) => {
-      return news.title.toLowerCase().includes(newsQuery.toLowerCase());
-    });
-
-    if (newsItem.length === 0) {
-      res.status(404).json({
-        message: "Berita tidak ditemukan!",
-      });
-    }
-    res.status(200).json(newsItem);
-  } else {
-    let newsItem2 = newsData.filter((news) => {
-      return news.title === newsParam;
-    });
-    if (newsItem2.length === 0) {
-      res.status(404).json({
-        message: "Berita tidak ditemukan!",
-      });
-    }
-    res.status(200).json(newsItem2);
-  }
-});
 
 module.exports = router;
