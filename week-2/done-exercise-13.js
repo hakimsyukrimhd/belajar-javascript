37eded89d5f7edaf432b0f5c18cb18d0203eee8b
Hakim Syukri <hakimsyukri.mhd@gmail.com>
2025-06-03 15:54:10 +0700

    update

--- week-2/done-exercise-13.js
+++ week-2/done-exercise-13.js
@@ -5,32 +5,38 @@
 // Code
 
 function xo(str) {
-    // step by step
-    // telusuri stringnya untuk mengecek isinya
-    let hurufO = "";
-    let hurufX = "";
-    for(i = 0; i < str.length; i++){
-        // jika isi dari string itu ada huruf o, maka simpan huruf o ke dalam variabel
-        if(str[i] === "o"){
-            hurufO += str[i];
-        // jika isi string itu ada huruf x, maka siimpan huruf x ke dalam yang variabel yang berbeda
-        }else if(str[i] === "x"){
-            hurufX += str[i];
-        }
+  // step by step
+  // telusuri stringnya untuk mengecek isinya
+  let hurufO = "";
+  let hurufX = "";
+  for (i = 0; i < str.length; i++) {
+    // jika isi dari string itu ada huruf o, maka simpan huruf o ke dalam variabel
+    if (str[i] === "o") {
+      hurufO += str[i];
+      // jika isi string itu ada huruf x, maka siimpan huruf x ke dalam yang variabel yang berbeda
+    } else if (str[i] === "x") {
+      hurufX += str[i];
     }
-    
-    if(hurufO.length === hurufX.length){
-        return true;
-    } else{
-        return false;
-    }
+  }
 
-    // setelah itu, bandingkan jumlah car o dan x,
+  if (hurufO.length === hurufX.length) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // setelah itu, bandingkan jumlah car o dan x,
 }
-  
+
 // TEST CASES
-console.log(xo('oxooxo')); // false
-console.log(xo('oxo')); // false
-console.log(xo('xxxooo')); // true
-console.log(xo('xoxooxxo')); // true
-console.log(xo('xoxoxo')); // true
+console.log(xo("oxooxo")); // false
+console.log(xo("oxo")); // false
+console.log(xo("xxxooo")); // true
+console.log(xo("xoxooxxo")); // true
+console.log(xo("xoxoxo")); // true
+
+const title = "Hari ini adalah Hari jumat";
+
+const slug = title.toLowerCase().split(" ").join("-");
+
+console.log(slug);
